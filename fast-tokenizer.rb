31d28846a4ab4a77fecd8627c9fd4eeca2ff8659
Dan Swain <dan@simpli.fi>
2012-08-11 12:36:05 -0500

    Kinda working

--- fast-tokenizer.rb
+++ fast-tokenizer.rb
@@ -1,21 +1,22 @@
 class FastToken
-  attr_accessor :name, :string, :opts
+  attr_accessor :name, :string, :opts, :breakpoints
+
+  def extract?
+    @opts[:extract]
+  end
 end
 
 class FastField < FastToken
   def initialize(name, opts)
     @name = name
-    @opts = opts
-  end
-
-  def extract?
-    opts[:extract]
+    @opts = {:extract => true}.merge(opts)
   end
 end
 
 class FastSeparator < FastToken
   def initialize(string)
     @string = string
+    @opts = {}
   end
 end
 
@@ -43,18 +44,31 @@ class FastTokenizer
     end
   end
 
-  def find_end_of token, string
-    0
+  def find_end_of token, string, start_at
+    start = string.index(token.string, start_at) || string.length
+    [start, [start + token.string.length, string.length].min]
   end
 
   def find_breakpoints(string)
     tokens.inject([]) do |breakpoints, t|
-      breakpoints << find_end_of t, string
-    end
+      start_point = breakpoints.size > 0 ? breakpoints.last.last : 0
+      breakpoints << if t.string.nil?
+                       [start_point]
+      else
+        find_end_of(t, string, start_point)
+      end
+    end.flatten.uniq
   end
 
   def tokenize! string
-    breakpoints = fink_breakpoints(string)
+    breakpoints = find_breakpoints(string)
+    breakpoints[0...-1].each_index do |i|
+      tokens[i].breakpoints = [breakpoints[i], breakpoints[i+1]]
+    end
+    tokens.select{|t| t.extract?}.inject([]) do |extracted_tokens, t|
+      extracted_string = string[t.breakpoints[0]...t.breakpoints[1]]
+      extracted_tokens << extracted_string
+    end
   end
 
 end
@@ -72,4 +86,12 @@ class NginxLogTokenizer < FastTokenizer
   look_for " "
 end
 
-puts NginxLogTokenizer.new.describe_line
+tokenizer = NginxLogTokenizer.new
+puts tokenizer.describe_line
+
+str = "foo - bar [fing] \"futs\" 1234 asdfasdf asdf "
+puts str
+puts tokenizer.find_breakpoints(str).inspect
+puts tokenizer.tokenize!(str).inspect
+
+
