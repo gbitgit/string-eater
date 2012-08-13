50156c35c68ca4fe0d6404b310b6c8fe05aa24d3
Dan Swain <dan@simpli.fi>
2012-08-13 13:34:28 -0500

    Add tests, work out some behavior

--- lib/string-eater.rb
+++ lib/string-eater.rb
@@ -38,7 +38,12 @@ module StringEater
     end
 
     def tokens
-      self.class.tokens
+      @tokens ||= self.class.tokens
+    end
+
+    def refresh_tokens
+      @tokens = nil
+      tokens
     end
 
     def describe_line
@@ -47,34 +52,42 @@ module StringEater
       end
     end
 
-    def find_end_of token, string, start_at
-      start = string.index(token.string, start_at) || string.length
-      [start, [start + token.string.length, string.length].min]
-    end
-
     def find_breakpoints(string)
-      tokens.inject([]) do |breakpoints, t|
-        start_point = breakpoints.size > 0 ? breakpoints.last.last : 0
-        breakpoints << if t.string.nil?
-                         [start_point]
-        else
+      breakpoints = tokens.select{|t| t.string }.inject([0]) do |breakpoints, t|
+        start_point = breakpoints.last
+        breakpoints.concat(
           find_end_of(t, string, start_point)
-        end
-      end.flatten.uniq
+        )
+      end
+      breakpoints << string.length unless breakpoints.last == string.length
     end
 
-    def tokenize! string
+    def tokenize! string, &block
       @extracted_tokens ||= {}
       @extracted_tokens.clear
+
       breakpoints = find_breakpoints(string)
       breakpoints[0...-1].each_index do |i|
         tokens[i].breakpoints = [breakpoints[i], breakpoints[i+1]]
       end
-      @extracted_tokens = tokens.select{|t| t.extract?}.
-        inject({}) do |extracted_tokens, t|
-        extracted_tokens[t.name] = string[t.breakpoints[0]...t.breakpoints[1]]
-        extracted_tokens
+
+      @tokens.select{|t| t.extract?}.each do |t|
+        @extracted_tokens[t.name] = string[t.breakpoints[0]...t.breakpoints[1]]
       end
+
+      if block_given?
+        yield @extracted_tokens
+      end
+      
+      # return self for chaining
+      self
+    end
+
+    protected
+
+    def find_end_of token, string, start_at
+      start = string.index(token.string, start_at) || string.length
+      [start, [start + token.string.length, string.length].min]
     end
 
   end
--- spec/string_eater_spec.rb
+++ spec/string_eater_spec.rb
@@ -1,8 +1,54 @@
 require 'spec_helper'
 require 'string-eater'
 
-describe StringEater::Tokenizer do
+# normal use
+class Example1 < StringEater::Tokenizer
+  add_field :first_word
+  look_for " "
+  add_field :second_word, :extract => false
+  look_for "|"
+  add_field :third_word
+end
 
-  
+describe Example1 do
+
+  before(:each) do
+    @tokenizer = Example1.new
+    @str1 = "foo bar|baz"
+    @first_word1 = "foo"
+    @third_word1 = "baz"
+    @bp1 = [0, 3,4,7,8,11]
+  end
+
+  describe "find_breakpoints" do
+    it "should return an array of the breakpoints" do
+      @tokenizer.find_breakpoints(@str1).should == @bp1
+    end
+  end
+
+  describe "tokenize!" do
+    it "should itself" do
+      @tokenizer.tokenize!(@str1).should == @tokenizer
+    end
+
+    it "should set the first word" do
+      @tokenizer.tokenize!(@str1).first_word.should == "foo"
+    end
+
+    it "should set the third word" do
+      @tokenizer.tokenize!(@str1).third_word.should == "baz"
+    end
+
+    it "should not set the second word" do
+      @tokenizer.tokenize!(@str1).second_word.should be_nil
+    end
+
+    it "should yield a hash of tokens if a block is given" do
+      @tokenizer.tokenize!(@str1) do |tokens|
+        tokens[:first_word].should == "foo"
+      end
+    end
+
+  end
 
 end
