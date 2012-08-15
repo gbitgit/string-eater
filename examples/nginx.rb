e2a3713230779b6817d38d25c46692d94595a55b
Dan Swain <dan@simpli.fi>
2012-08-15 11:20:09 -0500

    Modify nginx example to be able to load it from other files

--- examples/nginx.rb
+++ examples/nginx.rb
@@ -30,18 +30,19 @@ class NginxLogTokenizer < StringEater::Tokenizer
   combine_fields :from => :request_verb, :to => :request_etc, :as => :request
 end
 
-tokenizer = NginxLogTokenizer.new
-puts tokenizer.describe_line
+if __FILE__ == $0
+  tokenizer = NginxLogTokenizer.new
+  puts tokenizer.describe_line
 
-str = '73.80.217.212 - - [01/Aug/2012:09:14:25 -0500] "GET /this_is_a_url HTTP/1.1" 304 152 "http://referrer.com" "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)" "-" "there could be" other "stuff here"'
-puts "input string: " + str
-puts "found breakpoints: " + tokenizer.find_breakpoints(str).inspect
-puts "Tokens: "
-tokenizer.tokenize!(str) do |tokens|
-  tokens.each do |token|
-    puts "\t" + token.inspect
+  str = '73.80.217.212 - - [01/Aug/2012:09:14:25 -0500] "GET /this_is_a_url HTTP/1.1" 304 152 "http://referrer.com" "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)" "-" "there could be" other "stuff here"'
+  puts "input string: " + str
+  puts "found breakpoints: " + tokenizer.find_breakpoints(str).inspect
+  puts "Tokens: "
+  tokenizer.tokenize!(str) do |tokens|
+    tokens.each do |token|
+      puts "\t" + token.inspect
+    end
   end
+
+  puts tokenizer.ip
 end
-
-puts tokenizer.ip
-
