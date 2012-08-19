8687e737220ef96d1ef9dce14cac6b5674527376
Dan Swain <dan.t.swain@gmail.com>
2012-08-19 11:54:44 -0500

    Working version

--- ext/string-eater/c-tokenizer.c
+++ ext/string-eater/c-tokenizer.c
@@ -1,7 +1,76 @@
 #include <ruby.h>
 
+#define puts_inspect(var) \
+  ID inspect = rb_intern("inspect"); \
+  VALUE x = rb_funcall(var, inspect, 0); \
+  printf("%s\n", StringValueCStr(x));
+
+#define USE_METHOD(name) \
+  static ID name = 0; \
+  if(!name){ name = rb_intern(#name); }
+
 static VALUE rb_cCTokenizer;
 static VALUE rb_mStringEater;
+
+static void set_token_startpoint(VALUE self, long token_ix, size_t ix)
+{
+  USE_METHOD(set_token_startpoint);
+  rb_funcall(self, 
+      set_token_startpoint,
+      2, /* # args */
+      INT2FIX(token_ix),
+      INT2FIX(ix));
+}
+
+static void set_token_endpoint(VALUE self, long token_ix, size_t ix)
+{
+  USE_METHOD(set_token_endpoint);
+  rb_funcall(self, 
+      set_token_endpoint,
+      2, /* # args */
+      INT2FIX(token_ix),
+      INT2FIX(ix));
+}
+
+static unsigned char should_extract_token(VALUE self, long token_ix)
+{
+  static ID m_extract_token = 0;
+  if(!m_extract_token){
+    m_extract_token = rb_intern("extract_token?");
+  }
+  return RTEST(rb_funcall(self,
+      m_extract_token,
+      1, /* # args */
+      INT2FIX(token_ix)));
+}
+
+static size_t get_token_startpoint(VALUE self, unsigned int token_ix)
+{
+  USE_METHOD(get_token_startpoint);
+  return FIX2UINT(rb_funcall(self, 
+        get_token_startpoint,
+        1,
+        INT2FIX(token_ix)));
+}
+
+static void extract_token(VALUE self,
+    VALUE names,
+    unsigned int token_ix,
+    size_t endpoint,
+    const char* input_string)
+{
+  USE_METHOD(extract_token);
+
+  /* need to fetch the startpoint */
+  size_t startpoint = get_token_startpoint(self, token_ix);
+
+  rb_funcall(self,
+      extract_token,
+      2,
+      rb_ary_entry(names, token_ix),
+      rb_usascii_str_new(input_string + startpoint,
+        endpoint - startpoint));
+}
 
 static VALUE tokenize_string(VALUE self, 
     VALUE string,
@@ -13,7 +82,7 @@ static VALUE tokenize_string(VALUE self,
   const char* input_string = StringValueCStr(string);
   VALUE extracted_tokens = rb_hash_new();
   VALUE curr_token;
-  long curr_token_ix;
+  unsigned int curr_token_ix;
   long n_tokens_to_find = RARRAY_LEN(tokens_to_find_indexes);
   long n_tokens_to_extract = RARRAY_LEN(tokens_to_extract_indexes);
   size_t str_len = strlen(input_string);
@@ -24,9 +93,10 @@ static VALUE tokenize_string(VALUE self,
   size_t looking_for_ix = 0;
   long find_ix = 0;
   const char*  looking_for_token;
+  unsigned int n_tokens = (unsigned int)RARRAY_LEN(rb_iv_get(self, "@tokens"));
 
   curr_token = rb_ary_entry(tokens_to_find_strings, find_ix);
-  curr_token_ix = rb_ary_entry(tokens_to_find_indexes, find_ix);
+  curr_token_ix = NUM2UINT(rb_ary_entry(tokens_to_find_indexes, find_ix));
   looking_for_token = StringValueCStr(curr_token);
   looking_for_len = strlen(looking_for_token);
   looking_for = looking_for_token[looking_for_ix];
@@ -34,23 +104,45 @@ static VALUE tokenize_string(VALUE self,
   for(ix = 0; ix < str_len; ix++)
   {
     c = input_string[ix];
-    printf("'%c' == '%c'?\n", c, looking_for);
+    // printf("'%c' == '%c'?\n", c, looking_for);
+    // printf("find_ix = %d, curr_token_ix = %d\n", find_ix, curr_token_ix);
     if(c == looking_for)
     {
-      printf("Yes\n");
+      // printf("Yes\n");
       if(looking_for_ix == 0)
       {
         /* entering new token */
         if(curr_token_ix > 0)
         {
           /* set breakpoints, decide if we need to extract */
+          set_token_endpoint(self, curr_token_ix - 1, ix);
+          if(should_extract_token(self, curr_token_ix - 1))
+          {
+            /* need to fetch the startpoint */
+            size_t startpoint = get_token_startpoint(self, curr_token_ix - 1);
+
+            rb_hash_aset(extracted_tokens,
+                rb_ary_entry(tokens_to_extract_names, curr_token_ix - 1),
+                rb_usascii_str_new(input_string + startpoint,
+                  ix - startpoint));
+          }
         }
+        set_token_startpoint(self, curr_token_ix, ix);
       }
       if(looking_for_ix >= looking_for_len - 1)
       {
-        /* leaving token */
-
         /* set breakpoints */
+        set_token_endpoint(self, curr_token_ix, ix);
+        /* leaving token */
+        if(curr_token_ix >= n_tokens-1)
+        {
+          break;
+        }
+        else
+        {
+          set_token_startpoint(self, curr_token_ix + 1, ix + 1);
+        }
+
 
         /* next token */
         find_ix++;
@@ -59,8 +151,9 @@ static VALUE tokenize_string(VALUE self,
           /* done! */
           break;
         }
+
         curr_token = rb_ary_entry(tokens_to_find_strings, find_ix);
-        curr_token_ix = rb_ary_entry(tokens_to_find_indexes, find_ix);
+        curr_token_ix = NUM2UINT(rb_ary_entry(tokens_to_find_indexes, find_ix));
         looking_for_token = StringValueCStr(curr_token);
         looking_for_len = strlen(looking_for_token);
         looking_for_ix = 0;
@@ -71,6 +164,19 @@ static VALUE tokenize_string(VALUE self,
       }
       looking_for = looking_for_token[looking_for_ix];
     }
+  }
+
+  ix = str_len;
+  curr_token_ix = n_tokens - 1;
+  set_token_endpoint(self, curr_token_ix, ix);
+
+  if(should_extract_token(self, curr_token_ix))
+  {
+    size_t startpoint = get_token_startpoint(self, curr_token_ix);
+    rb_hash_aset(extracted_tokens,
+        rb_ary_entry(tokens_to_extract_names, curr_token_ix),
+        rb_usascii_str_new(input_string + startpoint,
+          ix - startpoint));
   }
 
   return extracted_tokens;
--- lib/c-tokenizer.rb
+++ lib/c-tokenizer.rb
@@ -51,7 +51,7 @@ class StringEater::CTokenizer
     end.compact
 
     @tokens_to_extract_indexes = tokens_to_extract.map{|t| t[0]}
-    @tokens_to_extract_names = tokens_to_extract.map{|t| t[1]}
+    @tokens_to_extract_names = tokens.map{|t| t.name}
 
     @combined_tokens = nil
   end
@@ -70,15 +70,56 @@ class StringEater::CTokenizer
     end.flatten.uniq
   end
 
+  def set_token_startpoint ix, startpoint
+    @tokens[ix].breakpoints[0] = startpoint
+  end
+
+  def get_token_startpoint ix
+    @tokens[ix].breakpoints[0]
+  end
+
+  def set_token_endpoint ix, endpoint
+    @tokens[ix].breakpoints[1] = endpoint
+  end
+
+  def extract_token? ix
+    @tokens[ix].extract?
+  end
+
   def tokenize! string, &block
     @string = string
     @extracted_tokens ||= {}
     @extracted_tokens.clear
+
+    tokens.first.breakpoints[0] = 0
 
     @extracted_tokens = ctokenize!(@string, 
                                    @tokens_to_find_indexes,
                                    @tokens_to_find_strings,
                                    @tokens_to_extract_indexes,
                                    @tokens_to_extract_names)
+
+#   last_token = tokens.last
+#   last_token.breakpoints[1] = string.length
+
+#   if last_token.extract?
+#     puts "last_token"
+#     puts last_token.breakpoints.inspect
+#     @extracted_tokens[last_token.name] = string[last_token.breakpoints[0]..last_token.breakpoints[1]]
+#   end
+
+    combined_tokens.each do |combiner|
+      name = combiner[0]
+      from = @tokens[combiner[1]].breakpoints[0]
+      to = @tokens[combiner[2]].breakpoints[1]
+      @extracted_tokens[name] = string[from...to]
+    end
+
+    if block_given?
+      yield @extracted_tokens
+    end
+
+    # return self for chaining
+    self
   end 
 end
